9fdb99874a8e1b6e2d82ef03f77237d578af0431
Gaya Sebastien <sebastien.gaya@etu.u-bordeaux.fr>
2019-04-02 14:47:33 +0200

    No more welcome

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
