aed8248881f02bce1632406f7606f8ec413698a4
Gaya Sebastien <sebastien.gaya@etu.u-bordeaux.fr>
2019-04-04 13:31:45 +0200

    HTTPS on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('FORCE_SSL')) {
+            $url->formatScheme('https');
+        }
     }
 }
